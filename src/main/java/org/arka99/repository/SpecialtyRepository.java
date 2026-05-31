9aa82f022c3a99793b914eb86011ec349b5035bb
Arka Bhuiyan <belmont66@duck.com>
2026-05-31 16:00:02 +0600

    Add SpecialtyRepository with queries for CRUD operations

---
+++ src/main/java/org/arka99/repository/SpecialtyRepository.java
@@ -0,0 +1,27 @@
+package org.arka99.repository;
+
+import io.micronaut.data.annotation.Query;
+import io.micronaut.data.annotation.Repository;
+import io.micronaut.data.jpa.repository.JpaRepository;
+import org.arka99.model.dto.response.SpecialtyResponse;
+import org.arka99.model.entity.Specialty;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface SpecialtyRepository extends JpaRepository<Specialty, Long> {
+
+    @Query(value = """
+        SELECT specialty.id,
+               specialty.name
+        FROM
+               Specialty specialty""")
+    List<SpecialtyResponse> findAllSpecialties();;
+
+    void deleteByName(String name);
+
+    Optional<Specialty> findByName(String name);
+
+    Boolean existsByName(String name);
+}
