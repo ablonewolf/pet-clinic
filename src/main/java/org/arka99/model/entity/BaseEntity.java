601264c744d8b870210f23ff6951058bd512f821
Arka Bhuiyan <belmont66@duck.com>
2026-05-31 15:57:11 +0600

    Add BaseEntity with audit fields for creation and update timestamps.

---
+++ src/main/java/org/arka99/model/entity/BaseEntity.java
@@ -0,0 +1,26 @@
+package org.arka99.model.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@MappedSuperclass
+public class BaseEntity {
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+}
