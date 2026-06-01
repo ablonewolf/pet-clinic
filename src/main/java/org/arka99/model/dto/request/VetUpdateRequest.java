d1f406fdcd4cbfdd3c4a89bc147cae15869eed86
Arka Bhuiyan <belmont66@duck.com>
2026-06-02 03:51:24 +0600

    Annotate `VetUpdateRequest.id` with `@Nonnull` for improved null-safety

--- src/main/java/org/arka99/model/dto/request/VetUpdateRequest.java
+++ src/main/java/org/arka99/model/dto/request/VetUpdateRequest.java
@@ -2,13 +2,17 @@ package org.arka99.model.dto.request;
 
 import io.micronaut.core.annotation.Introspected;
 import io.micronaut.serde.annotation.Serdeable;
+import jakarta.annotation.Nonnull;
 
 import java.util.List;
 
 @Introspected
 @Serdeable
-public record VetUpdateRequest(Long id,
-                               String firstName,
-                               String lastName,
-                               List<String> specialties) {
+public record VetUpdateRequest(
+    @Nonnull
+    Long id,
+
+    String firstName,
+    String lastName,
+    List<String> specialties) {
 }
