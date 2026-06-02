efee4f5b28662f275c7d2c13abeffd42385d3f23
Arka Bhuiyan <belmont66@duck.com>
2026-06-02 12:18:12 +0600

    Update `VetUpdateRequest`: replace `@Nonnull` with `@NotNull` and add `@NotBlank` for mandatory fields

--- src/main/java/org/arka99/model/dto/request/VetUpdateRequest.java
+++ src/main/java/org/arka99/model/dto/request/VetUpdateRequest.java
@@ -2,17 +2,21 @@ package org.arka99.model.dto.request;
 
 import io.micronaut.core.annotation.Introspected;
 import io.micronaut.serde.annotation.Serdeable;
-import jakarta.annotation.Nonnull;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 import java.util.List;
 
 @Introspected
 @Serdeable
 public record VetUpdateRequest(
-    @Nonnull
+    @NotNull
     Long id,
 
+    @NotBlank
     String firstName,
+
+    @NotBlank
     String lastName,
     List<String> specialties) {
 }
