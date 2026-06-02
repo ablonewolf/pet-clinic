506fc3c0d42662b719db591d32d5d4d77e2009c9
Arka Bhuiyan <belmont66@duck.com>
2026-06-02 16:55:33 +0600

    Add `findAllSpecialtiesByCursor` method in `SpecialtyRepository` for cursored pagination support

--- src/main/java/org/arka99/repository/SpecialtyRepository.java
+++ src/main/java/org/arka99/repository/SpecialtyRepository.java
@@ -3,6 +3,8 @@ package org.arka99.repository;
 import io.micronaut.data.annotation.Query;
 import io.micronaut.data.annotation.Repository;
 import io.micronaut.data.jpa.repository.JpaRepository;
+import io.micronaut.data.model.CursoredPage;
+import io.micronaut.data.model.CursoredPageable;
 import io.micronaut.data.model.Page;
 import io.micronaut.data.model.Pageable;
 import org.arka99.config.TraceThread;
@@ -25,6 +27,15 @@ public interface SpecialtyRepository extends JpaRepository<Specialty, Long> {
         FROM Specialty specialty
         """)
     Page<SpecialtyResponse> findAllSpecialties(Pageable pageable);
+
+    @Query(value = """
+        SELECT specialty
+        FROM Specialty specialty
+        """, countQuery = """
+        SELECT COUNT(specialty)
+        FROM Specialty specialty
+        """)
+    CursoredPage<Specialty> findAllSpecialtiesByCursor(CursoredPageable pageable);
 
     void deleteByName(String name);
 
